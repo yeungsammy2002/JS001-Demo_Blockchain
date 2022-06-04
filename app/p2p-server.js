410b3148cda32ef170816ef6f00ae0e5aba4fabc
Sammy Yeung <yeungsammy2002@yahoo.com.hk>
2022-06-04 21:15:32 +0800

    -

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,19 +2,7 @@ const WebSocket = require("ws");
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 
-// "peers" is used to check if peer environment variable has been declared
-// we assumes that this "peers" environment variable is a string,
-// contains a list of WebSocket addresses that this WebSocket should
-// connect to as a "peer"
-
-// For example, if there's an application running on 5001 as it's P2P_PORT,
-// ws://localhost:5001 is going to be its WebSocket addresses
-// for our "peers" environment variable, if there is more than one connect to,
-// you will combine them with string, i.e. PEERS is an environment variable
-// PEERS=ws://localhost:5001,ws://localhost:5002,...
-// later may run this command to customize the port of your blockchain app:
-// HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
-
+// peers is an array of peer's websocket addresses
 const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
 
 class P2pServer {
@@ -26,10 +14,9 @@ class P2pServer {
   // make sure the first blockchain application can open up a WebSocket server
   listen() {
     const server = new WebSocket.Server({ port: P2P_PORT });
-    // by listening a "connection" event,
-    // we can fire specific code whenever a new socket connects to this server
     server.on("connection", (socket) => {
-      // "socket" object that is created as the result of this connection
+      // any new instance try connect to this app,
+      // their sockets will be pushed to "this.sockets" array
       this.connectSocket(socket);
     });
     console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
@@ -57,9 +44,10 @@ class P2pServer {
   connectSocket(socket) {
     this.sockets.push(socket);
     console.log("Socket connected");
-
+    // any incoming data sent to this app,
+    // will invoke this.blockchain.replaceChain()
     this.messageHandler(socket);
-
+    // if the local chain is longer, send it back to the peer is useful
     this.sendChain(socket);
   }
 
